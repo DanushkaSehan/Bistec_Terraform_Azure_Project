e4409b051897681dd4f621627927ce3728e6c8e6
JAD Sehan <137585622+DanushkaSehan@users.noreply.github.com>
2025-03-21 15:51:08 +0530

    final commit

--- main.tf
+++ main.tf
@@ -1,26 +1,27 @@
 provider "azurerm" {
-  features {}
-resource_provider_registrations = "none"
-  subscription_id = "a2b28c85-1948-4263-90ca-bade2bac4df4"
+  features {}  # Required for Azure provider
+
+  resource_provider_registrations = "none"  # No automatic resource provider registrations
+  subscription_id = "a2b28c85-1948-4263-90ca-bade2bac4df4"  # KodeKloud Azure sandbox default subscription
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "myAKSCluster"
-  location           = "East US"  
-  resource_group_name = "kml_rg_main-b01ac03244b34bf0"
-  dns_prefix         = "myaksdns"
+  name                = "myAKSClusterBistec"  # Name of the AKS cluster
+  location           = "West US"  # Azure region
+  resource_group_name = "kml_rg_main-d6c1db9800654916"  # Resource group name(in Kodecloud only have one resource group)
+  dns_prefix         = "myaksdns"  # DNS prefix for the cluster
 
   default_node_pool {
-    name       = "agentpool"
-    node_count = 2
-    vm_size    = "Standard_D2s_v3"
+    name       = "agentpool"  # Pool name
+    node_count = 2  # Number of nodes in the pool
+    vm_size    = "Standard_D2s_v3"  # VM size for the nodes
   }
 
   identity {
-    type = "SystemAssigned"
+    type = "SystemAssigned"  # Used system assigned managed identity
   }
 
   tags = {
-    environment = "dev"
+    environment = "dev"  # Taging for environment classification
   }
 }
